ef7571174d2bb672965ef63273e0b84a32193210
Rimpy Mittal <88575365+Rimpy212@users.noreply.github.com>
2024-08-26 15:09:19 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (48.15%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,28 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if(n<=2){
-            return n;
-        }
-
-        int j = 2;
-        for(int i=2; i<n; i++){
-            if(nums[i] != nums[j-2]){
-                nums[j] = nums[i];
-                j++;
+        int n=nums.length;
+        int k=1;
+        int count=1;
+        int finalCount=1;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                nums[k]=nums[i];
+                k++;
+                count=1;
+                finalCount++;
+            }
+            else{
+                count++;
+                if(count<=2)
+                {
+                    nums[k]=nums[i];
+                    k++;
+                    finalCount++;
+                }
             }
         }
-        return j;
+        return finalCount;
     }
 }
